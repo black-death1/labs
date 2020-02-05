b39aca918d7fcab11799d122b7c7c0ea5306f980
ahmad haboukh <ahmadhaboukh616@gmail.com>
2020-02-05 23:53:52 +0200

    aded labs 4 and 5 and 6

---
+++ javascript/java.js
@@ -0,0 +1,28 @@
+// var car = do you own a car()
+// console.log(car)
+// var age = are you above the age of 18()
+// console.log(age)
+
+var car =  prompt("do you own a car?");
+
+var grettngs;
+
+if (car = yes) {
+    grettngs = 'welcome!';
+}
+else if (car = no) {
+    grettngs = 'bad!';
+}
+
+var age = prompt("are you above the age of 18?");
+
+var grettngs;
+
+if (age = yes) {
+    grettngs = 'welcome in my website!'
+}
+if (age = no) {
+    gretings = 'bad!'
+}
+
+document.write('<h3>'+grettngs+'</h3>');
